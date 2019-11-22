5751bf88218255a5fe88a1ebefac9d2f19ebe0bc
P.M <vyfullspam50@gmail.com>
2019-11-22 16:51:14 +0200

    Add files via upload
    Person
    You are asked to model an application for storing data about people. You should be able to have a person and a child. The child derives from the person. Your task is to model the application. The only constraints are:
    People should not be able to have a negative age
    Children should not be able to have an age more than 15.
    
    Person – represents the base class by which all of the others are implemented
    Child - represents a class, which derives from Person.
    Note
    Your class’s names MUST be the same as the names shown above!!!
     
    Sample Main()	   
    static void Main()
    {
        string name = Console.ReadLine();
        int age = int.Parse(Console.ReadLine());
        
        Child child = new Child(name, age);
        Console.WriteLine(child);
    }	 
    Create a new empty class and name it Person. Set its access modifier to public so it can be instantiated from any project. Every person has a name, and an age.
     
    Sample Code	   
    public class Person
    {
       // 1. Add Fields
    
       // 2. Add Constructor
    
       // 3. Add Properties
    
       // 4. Add Methods
    }	 
    Define a field for each property the class should have (e.g. Name, Age) 
    Define the Name and Age properties of a Person. 
    Step 1 - Define a Constructor
    Define a constructor that accepts name and age.
     
    Sample Code	   
    public Person(string name, int age)
    {
        this.Name = name;
        this.Age = age;
    }	 
    Step 2 - Override ToString()
    As you probably already know, all classes in C# inherit the Object class and therefore have all its public members (ToString(), Equals() and GetHashCode() methods). ToString() serves to return information about an instance as string. Let's override (change) its behavior for our Person class.
     
    Sample Code	   
    public override string ToString()
    {
        StringBuilder stringBuilder = new StringBuilder();
        stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
                             this.Name,
                             this.Age));
    
        return stringBuilder.ToString();
    }	 
    And voila! If everything is correct, we can now create Person objects and display information about them.

---
+++ Inheritance/01-Person/Child.cs
@@ -0,0 +1,13 @@
+﻿
+namespace _01_Person
+{
+using System;
+using System.Collections.Generic;
+using System.Text;
+    public class Child : Person
+    {
+        public Child(string name, int age) : base(name, age)
+        {
+        }
+    }
+}
