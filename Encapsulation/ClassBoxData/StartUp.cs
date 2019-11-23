c253344112ed42ed71b158edc844f29804171c1a
P.M <vyfullspam50@gmail.com>
2019-11-23 23:53:46 +0200

    Add files via upload
    Class Box Data
    You are given a geometric figure box with parameters length, width and height. Model a class Box that that can be instantiated by the same three parameters. Expose to the outside world only methods for its surface area, lateral surface area and its volume (formulas: http://www.mathwords.com/r/rectangular_parallelepiped.htm).
    A box’s side should not be zero or a negative number. Аdd data validation for each parameter given to the constructor. Make a private setter that performs data validation internally. 
    Input
    On the first three lines you will get the length, width and height. 
    Output
    On the next three lines print the surface area, lateral surface area and the volume of the box:
    Examples
     
    Input	Output	   
    2
    3
    4	Surface Area - 52.00
    Lateral Surface Area - 40.00
    Volume - 24.00	   
    1.3
    1
    6	Surface Area - 30.20
    Lateral Surface Area - 27.60
    Volume - 7.80	   
    2
    -3
    4	Width cannot be zero or negative.

---
+++ Encapsulation/ClassBoxData/StartUp.cs
@@ -0,0 +1,100 @@
+﻿
+namespace ClassBoxData
+{
+using System;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var line1 = double.Parse(Console.ReadLine());
+            var line2 = double.Parse(Console.ReadLine());
+            var line3 = double.Parse(Console.ReadLine());
+
+            Box box = null;
+
+            try
+            {
+                box = new Box(line1, line2, line3);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            box.SurfaceArea();
+            box.LateralSurfaceArea();
+            box.Volume();
+
+        }
+    }
+
+    class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Height = height;
+            Width = width;
+            Length = length;
+        }
+
+        public void SurfaceArea()
+        {
+            var surfaceArea = 2 * Length * Width + 2 * Length * Height + 2 * Width * Height;
+            Console.WriteLine($"Surface Area - {surfaceArea:F2}");
+        }
+
+        public void LateralSurfaceArea()
+        {
+            var LateralSurfaceArea = 2 * Length * Height + 2 * Width * Height;
+            Console.WriteLine($"Lateral Surface Area - {LateralSurfaceArea:F2}");
+        }
+
+        public void Volume()
+        {
+            var volume = Length * Height * Width;
+            Console.WriteLine($"Volume - {volume:F2}");
+        }
+        public double Height
+        {
+            get { return height; }
+            set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            private set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+
+        public double Length 
+        { 
+            get =>  this.length; 
+            private set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+    }
+}
