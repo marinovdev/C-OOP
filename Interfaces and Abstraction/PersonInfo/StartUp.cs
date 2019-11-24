b664f1512b025e022d26c7f7dedf1a5cf21845f8
P.M <vyfullspam50@gmail.com>
2019-11-24 23:47:08 +0200

    Add files via upload
    Multiple Implementation
    NOTE: You need a public StartUp class with the namespace PersonInfo.
    Using the code from the previous task, define an interface IIdentifiable with a string property Id and an interface IBirthable with a string property Birthdate and implement them in the Citizen class. Rewrite the Citizen constructor to accept the new parameters.
    Test your class like this:
     
    string name = Console.ReadLine();
    int age = int.Parse(Console.ReadLine());
    string id = Console.ReadLine();
    string birthdate = Console.ReadLine();
    IIdentifiable identifiable = new Citizen(name, age,id, birthdate);
    IBirthable birthable = new Citizen(name, age, id, birthdate);
    Console.WriteLine(identifiable.Id);
    Console.WriteLine(birthable.Birthdate);	 
    Examples
     
    Input	Output	   
    Pesho
    25
    9105152287
    15/05/1991	9105152287
    15/05/1991

---
+++ Interfaces and Abstraction/PersonInfo/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string id = Console.ReadLine();
+            string birthdate = Console.ReadLine();
+            IIdentifiable identifiable = new Citizen(name, age, id, birthdate);
+            IBirthable birthable = new Citizen(name, age, id, birthdate);
+            Console.WriteLine(identifiable.Id);
+            Console.WriteLine(birthable.Birthdate);
+
+        }
+    }
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            Name = name;
+            Age = age;
+            Birthdate = birthdate;
+            Id = id;
+        }
+
+        public string Name { get ; set ; }
+        public int Age { get; set; }
+        public string Birthdate { get; set; }
+        public string Id { get; set; }
+    }
+
+    public interface IIdentifiable
+    {
+        string Id { get; set; }
+    }
+
+    public interface IBirthable
+    {
+        string Birthdate { get; set; }
+    }
+    public interface IPerson
+    {
+         string Name { get; set; }
+
+         int Age { get; set; }
+    }
+}
