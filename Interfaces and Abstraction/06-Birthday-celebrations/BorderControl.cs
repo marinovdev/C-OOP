631980a690395d8ab64c1612c772494645f70699
P.M <vyfullspam50@gmail.com>
2019-11-25 13:42:22 +0200

    Add files via upload
    Birthday Celebrations
    It is a well known fact that people celebrate birthdays, it is also known that some people also celebrate their pets’ birthdays. Extend the program from your last task to add birthdates to citizens and include a class Pet, pets have a name and a birthdate. Encompass repeated functionality into interfaces and implement them in your classes. 
    You will receive from the console an unknown amount of lines. Until the command "End" is received, each line will contain information in one of the following formats "Citizen <name> <age> <id> <birthdate>" for Citizen, "Robot <model> <id>" for Robot or "Pet <name> <birthdate" for Pet. After the "End" command on the next line you will receive a single number representing a specific year, your task is to print all birthdates (of both Citizen and Pet) in that year in the format day/month/year in the order of input.
    Examples
     
    Input	Output	   
    Citizen Pesho 22 9010101122 10/10/1990
    Pet Sharo 13/11/2005
    Robot MK-13 558833251
    End
    1990	10/10/1990	   
    Citizen Stamat 16 0041018380 01/01/2000
    Robot MK-10 12345678
    Robot PP-09 00000001
    Pet Topcho 24/12/2000
    Pet Kosmat 12/06/2002 
    End
    2000	01/01/2000
    24/12/2000	   
    Robot VV-XYZ 11213141
    Citizen Penka 35 7903210713 21/03/1979
    Citizen Kane 40 7409073566 07/09/1974
    End
    1975	<empty output>

---
+++ Interfaces and Abstraction/06-Birthday-celebrations/BorderControl.cs
@@ -0,0 +1,161 @@
+﻿
+namespace BorderControl
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class BorderControl
+    {
+        public static List<string> detained = new List<string>();
+        public static List<string> birthYearList = new List<string>();
+        public static List<Entity> entities = new List<Entity>();
+        public static void Main()
+        {
+            var command = "";
+            while ((command = Console.ReadLine()) != "End")
+            {
+                var args = command
+                    .Split();
+                var name = "";
+                var age = 0;
+                var id = "";
+                var birthdate = "";
+
+                if(args[0] == "Citizen")
+                {
+                    name = args[1];
+                    age = int.Parse(args[2]);
+                    id = args[3];
+                    birthdate = args[4];
+
+                    var citizen = new Citizen(name, age, id, birthdate);
+
+                    entities.Add(citizen);
+                }
+                else if(args[0] == "Robot")
+                {
+                    name = args[1];
+                    id = args[2];
+                    var robot = new Robot(name, id);
+
+                    entities.Add(robot);
+                }
+                else if (args[0] == "Pet")
+                {
+                    name = args[1];
+                    birthdate = args[2];
+
+                    var pet = new Pet(name, birthdate);
+
+                    entities.Add(pet);
+                }
+            }
+
+            var suffix = Console.ReadLine();
+
+            foreach (var item in entities)
+            {
+                item.SortByBirthYear(suffix);
+            }
+
+            if (birthYearList.Count > 0)
+            {
+                foreach (var item in birthYearList)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+        public interface IBorn
+        {
+            string Birthdate { get; set; }
+
+            void SortByBirthYear(string year);
+        }
+        public class Entity : IBorn
+        {
+            public virtual void CheckId(string suffix)
+            {
+                if (this.Id.ToString().EndsWith(suffix))
+                {
+                    BorderControl.detained.Add(this.Id);
+                }
+            }
+
+            public virtual void SortByBirthYear(string year)
+            {
+            }
+
+            public virtual string Name { get; set; }
+
+            public virtual string Id { get; set; }
+            public virtual string Birthdate { get; set; }
+        }
+
+        public class Pet : Entity
+        {
+            public Pet(string name, string birthdate)
+            {
+                Name = name;
+                Birthdate = birthdate;
+            }
+
+            public override string Name { get; set; }
+            public override string Birthdate { get; set; }
+
+            public override void SortByBirthYear(string year)
+            {
+                if (this.Birthdate.ToString().EndsWith(year))
+                {
+                    BorderControl.birthYearList.Add(this.Birthdate);
+                }
+            }
+        }
+        public class Robot : Entity, IBorn
+        {
+            public Robot(string name, string id)
+            {
+                Name = name;
+                Id = id;
+            }
+
+            public override string Name { get; set; }
+
+            public override string Id { get; set; }
+            public override string Birthdate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public override void SortByBirthYear(string year)
+            {
+            }
+        }
+        public class Citizen : Entity
+        {
+            public Citizen(string name, int age, string id, string birthdate)
+            {
+                Name = name;
+                Age = age;
+                Id = id;
+                Birthdate = birthdate;
+            }
+
+            public override void SortByBirthYear(string year)
+            {
+                    if (this.Birthdate.ToString().EndsWith(year))
+                    {
+                        BorderControl.birthYearList.Add(this.Birthdate);
+                    }
+            }
+
+            public override string Name { get; set; }
+
+            public int Age { get; set; }
+
+            public override string Id { get; set; }
+
+            public override string Birthdate { get; set; }
+
+        }
+    }
+}
