3bcae1d57bd879f6160747ee022898fb6ef9d388
P.M <vyfullspam50@gmail.com>
2019-11-25 02:07:52 +0200

    Add files via upload
    Border Control
    It’s the future, you’re the ruler of a totalitarian dystopian society inhabited by citizens and robots, since you’re afraid of rebellions you decide to implement strict control of who enters your city. Your soldiers check the Ids of everyone who enters and leaves.
    You will receive an unknown amount of lines from the console until the command "End" is received, on each line there will be a piece of information for either a citizen or a robot who tries to enter your city in the format: "{name} {age} {id}" for citizens and "{model} {id}" for robots. After the end command on the next line you will receive a single number representing the last digits of fake ids, all citizens or robots whose Id ends with the specified digits must be detained.
    The output of your program should consist of all detained Ids each on a separate line in the order of input.
    Input
    The input comes from the console. Every commands’ parameters before the command "End" will be separated by a single space.
    Examples
     
    Input	Output	   
    Pesho 22 9010101122
    MK-13 558833251
    MK-12 33283122
    End
    122	9010101122
    33283122
    	   
    Toncho 31 7801211340
    Penka 29 8007181534
    IV-228 999999
    Stamat 54 3401018380
    KKK-666 80808080
    End
    340	7801211340

---
+++ Interfaces and Abstraction/BorderControl/BorderControl.cs
@@ -0,0 +1,101 @@
+﻿
+namespace BorderControl
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BorderControl
+    {
+        public static List<string> detained = new List<string>();
+        public static List<Entity> entities = new List<Entity>();
+        public static void Main()
+        {
+            var command = "";
+            while ((command = Console.ReadLine()) != "End")
+            {
+                var args = command
+                    .Split();
+
+                var name = "";
+                var model = "";
+                var age = 0;
+                var id = "";
+
+                if (args.Length == 3)
+                {
+                    name = args[0];
+                    age = int.Parse(args[1]);
+                    id = args[2];
+
+                    var citizen = new Citizen(name, age, id);
+
+                    entities.Add(citizen);
+                }
+                else
+                {
+                    model = args[0];
+                    id = args[1];
+                    var robot = new Robot(model, id);
+                    entities.Add(robot);
+                }
+
+            }
+
+            var suffix = Console.ReadLine();
+
+            foreach (var item in entities)
+            {
+                item.CheckId(suffix);
+
+            }
+
+            if (detained.Count > 0)
+            {
+                foreach (var item in detained)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+        public class Entity
+        {
+            public virtual void CheckId(string suffix)
+            {
+                if (this.Id.ToString().EndsWith(suffix))
+                {
+                    BorderControl.detained.Add(this.Id);
+                }
+            }
+
+            public virtual string Id { get; set; }
+        }
+        public class Robot : Entity
+        {
+            public Robot(string model, string id)
+            {
+                Model = model;
+                Id = id;
+            }
+
+            public string Model { get; set; }
+
+            public override string Id { get; set; }
+        }
+        public class Citizen : Entity
+        {
+            public Citizen(string name, int age, string id)
+            {
+                Name = name;
+                Age = age;
+                Id = id;
+            }
+
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+
+            public override string Id { get; set; }
+        }
+    }
+}
