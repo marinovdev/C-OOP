8422496bf6e9535f2897e00bcc77d3fcea9145d6
P.M <vyfullspam50@gmail.com>
2019-11-25 14:57:34 +0200

    Add files via upload
    Food Shortage
    Your totalitarian dystopian society suffers a shortage of food, so many rebels appear. Extend the code from your previous task with new functionality to solve this task.
    Define a class Rebel which has a name, age and group (string), names are unique - there will never be 2 Rebels/Citizens or a Rebel and Citizen with the same name. Define an interface IBuyer which defines a method BuyFood() and an integer property Food. Implement the IBuyer interface in the Citizen and Rebel class, both Rebels and Citizens start with 0 food, when a Rebel buys food his Food increases by 5, when a Citizen buys food his Food increases by 10.
    On the first line of the input you will receive an integer N - the number of people, on each of the next N lines you will receive information in one of the following formats "<name> <age> <id> <birthdate>" for a Citizen or "<name> <age><group>" for a Rebel. After the N lines until the command "End" is received, you will receive names of people who bought food, each on a new line. Note that not all names may be valid, in case of an incorrect name - nothing should happen.  
    Output
    The output consists of only one line on which you should print the total amount of food purchased.
    Examples
     
    Input	Output	   
    2
    Pesho 25 8904041303 04/04/1989
    Stancho 27 WildMonkeys
    Pesho
    Gosho
    Pesho
    End	20	   
    4
    Stamat 23 TheSwarm
    Toncho 44 7308185527 18/08/1973
    Joro 31 Terrorists
    Penka 27 881222212 22/12/1988
    Jiraf
    Jo ro
    Jiraf
    Joro
    Stamat
    Penka
    End	20

---
+++ Interfaces and Abstraction/07-Food-Shortage/BorderControl.cs
@@ -0,0 +1,150 @@
+﻿
+namespace BorderControl
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class BorderControl
+    {
+        public static int TotalFood { get; set; } = 0;
+        public static List<IBuyer> entities = new List<IBuyer>();
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            while (n > 0)
+            {
+                var input = Console.ReadLine();
+
+                var args = input.Split();
+                var name = "";
+                var age = 0;
+                var id = "";
+                var birthdate = "";
+                var group = "";
+
+                if(args.Length == 4)
+                {
+                    name = args[0];
+                    age = int.Parse(args[1]);
+                    id = args[2];
+                    birthdate = args[3];
+
+                    var cit = new Citizen(name, age, id, birthdate);
+                    entities.Add(cit);
+                }
+                else if (args.Length == 3)
+                {
+                    name = args[0];
+                    age = int.Parse(args[1]);
+                    group = args[2];
+
+                    var rebel = new Rebel(name, age, group);
+                    entities.Add(rebel);
+                }
+                n--;
+            }
+
+            var command = "";
+            while((command = Console.ReadLine()) != "End")
+            {
+                if(entities.Any(e => e.Name == command))
+                {
+                    entities.Where(e => e.Name == command).First().BuyFood();
+                }
+            }
+
+            Console.WriteLine(TotalFood);
+        }
+
+
+        public interface IBuyer
+        {
+            void BuyFood();
+            int Food { get; set; }
+
+            string Name { get; set; }
+        }
+        public class Entity
+        {
+            public virtual string Name { get; set; }
+
+            public virtual string Id { get; set; }
+            public virtual string Birthdate { get; set; }
+        }
+
+        public class Pet : Entity
+        {
+            public Pet(string name, string birthdate)
+            {
+                Name = name;
+                Birthdate = birthdate;
+            }
+
+            public override string Name { get; set; }
+            public override string Birthdate { get; set; }
+
+        }
+        public class Robot : Entity
+        {
+            public Robot(string name, string id)
+            {
+                Name = name;
+                Id = id;
+            }
+
+            public override string Name { get; set; }
+
+            public override string Id { get; set; }
+
+        }
+
+        public class Rebel : Entity, IBuyer
+        {
+
+            public Rebel(string name, int age, string group)
+            {
+                Name = name;
+                this.Age = age;
+                this.Group = group;
+            }
+
+            public void BuyFood()
+            {
+                    this.Food += 5;
+                    BorderControl.TotalFood += 5;
+            }
+            public int Food { get; set; }
+
+            public int Age { get; set; }
+
+            public string Group { get; set; }
+        }
+
+        public class Citizen : Entity, IBuyer
+        {
+            public Citizen(string name, int age, string id, string birthdate)
+            {
+                Name = name;
+                Age = age;
+                Id = id;
+                Birthdate = birthdate;
+            }
+
+            public void BuyFood()
+            {
+                    this.Food += 10;
+                    BorderControl.TotalFood += 10;
+            }
+
+            public override string Name { get; set; }
+
+            public int Age { get; set; }
+
+            public override string Id { get; set; }
+
+            public override string Birthdate { get; set; }
+            public int Food { get; set; }
+        }
+    }
+}
