e16b2b55824ae28fc9045d3f5b64d120e7a0f665
P.M <vyfullspam50@gmail.com>
2019-11-26 14:48:43 +0200

    Add files via upload
    Vehicles
    Write a program that models 2 vehicles (a Car and a Truck) and simulates driving and refueling them. Car and Truck both have fuel quantity, fuel consumption in liters per km and can be driven a given distance and refueled with a given amount of fuel. It’s summer, so both vehicles use air conditioners and their fuel consumption per km is increased by 0.9 liters for the car and with 1.6 liters for the truck. Also, the Truck has a tiny hole in its tank and when its refueled it keeps only 95% of the given fuel. The car has no problems and adds all the given fuel to its tank. If a vehicle cannot travel the given distance, its fuel does not change.
    Input
    On the first line - information about the Car in the format: "Car {fuel quantity} {liters per km}"
    On the second line - info about the Truck in the format: "Truck {fuel quantity} {liters per km}"
    On the third line - the number of commands N that will be given on the next N lines
    On the next N lines - commands in the format:
    "Drive Car {distance}"
    "Drive Truck {distance}"
    "Refuel Car {liters}"
    "Refuel Truck {liters}"
    Output
    After each "Drive" command, if there was enough fuel, print on the console a message in the format:
    "Car/Truck travelled {distance} km"
    If there was not enough fuel, print: "Car/Truck needs refueling"
    After the "End" command, print the remaining fuel for both the car and the truck, rounded to 2 digits after the floating point in the format:
    "Car: {liters}"
    "Truck: {liters}"
    Examples
     
    Input	Output	   
    Car 15 0.3
    Truck 100 0.9
    4
    Drive Car 9
    Drive Car 30
    Refuel Car 50
    Drive Truck 10	Car travelled 9 km
    Car needs refueling
    Truck travelled 10 km
    Car: 54.20
    Truck: 75.00	   
    Car 30.4 0.4
    Truck 99.34 0.9
    5
    Drive Car 500
    Drive Car 13.5
    Refuel Truck 10.300
    Drive Truck 56.2
    Refuel Car 100.2	Car needs refueling
    Car travelled 13.5 km
    Truck needs refueling
    Car: 113.05
    Truck: 109.13

---
+++ Polymorphism/01-Vehicles/StartUp.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_Vehicles
+{
+    public abstract class Vehicle
+    {
+        protected Vehicle(double fuelQuantity, double fuelConsumption)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public abstract void Drive(double killometers);
+
+        public abstract void RefuelCar(double litters);
+
+        public abstract double FuelQuantity { get; set; }
+
+        public abstract double FuelConsumption { get; set; }
+
+        public abstract double FuelLoss { get;}
+
+    }
+
+    public class Car : Vehicle
+    {
+        public Car(double fuelQuantity, double fuelConsumption) 
+            : base(fuelQuantity, fuelConsumption)
+        {
+
+        }
+
+        public override double FuelQuantity { get; set; }
+        public override double FuelConsumption { get; set; }
+        public override double FuelLoss { get => 0.9;}
+
+        public override void Drive(double killometers)
+        {
+            if (this.FuelQuantity - (this.FuelConsumption + this.FuelLoss) * killometers >= 0)
+            {
+                this.FuelQuantity -= (this.FuelConsumption + this.FuelLoss) * killometers;
+                Console.WriteLine($"Car travelled {killometers} km");
+            }
+            else
+                Console.WriteLine($"Car needs refueling");
+        }
+
+        public override void RefuelCar(double litters)
+        {
+            this.FuelQuantity += litters;
+        }
+    }
+
+    public class Truck : Vehicle
+    {
+
+        private const double _refuilingLoss = 0.95;
+        public Truck(double fuelQuantity, double fuelConsumption)
+            : base(fuelQuantity, fuelConsumption)
+        {
+        }
+
+        public override double FuelQuantity { get; set; }
+        public override double FuelConsumption { get; set; }
+
+        public override double FuelLoss => 1.6;
+
+        public override void Drive(double killometers)
+        {
+            if (this.FuelQuantity - (this.FuelConsumption + this.FuelLoss) * killometers >= 0)
+            {
+                this.FuelQuantity -= (this.FuelConsumption + this.FuelLoss) * killometers;
+                Console.WriteLine($"Truck travelled {killometers} km");
+            }
+            else
+                Console.WriteLine($"Truck needs refueling");
+        }
+
+        public override void RefuelCar(double litters)
+        {
+            this.FuelQuantity += litters / 100 * 95;
+        }
+    }
+    public static class StartUp
+    {
+       public  static void Main()
+        {
+            var carParams = Console.ReadLine()
+                .Split();
+
+            var truckParams = Console.ReadLine()
+                .Split();
+
+            var car = new Car(double.Parse(carParams[1]), double.Parse(carParams[2]));
+            var truck = new Truck(double.Parse(truckParams[1]), double.Parse(truckParams[2]));
+
+            var iterations = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < iterations; i++)
+            {
+                var command = Console.ReadLine().Split();
+
+                var operation = command[0];
+                var vehicle = command[1];
+
+                if(operation == "Drive")
+                {
+                    var distance = double.Parse(command[2]);
+                    if (vehicle == "Car")
+                    {
+                        car.Drive(distance);
+                    }
+                    else if (vehicle == "Truck")
+                    {
+                        truck.Drive(distance);
+                    }
+                }
+                else
+                {
+                    var ammount = double.Parse(command[2]);
+                    if (vehicle == "Car")
+                    {
+                        car.RefuelCar(ammount);
+                    }
+                    else if (vehicle == "Truck")
+                    {
+                        truck.RefuelCar(ammount);
+                    }
+                }
+            }
+            var carFuel = Math.Round(car.FuelQuantity, 2, MidpointRounding.AwayFromZero);
+            var truckFuel = Math.Round(truck.FuelQuantity, 2, MidpointRounding.AwayFromZero);
+
+            Console.WriteLine($"Car: {carFuel:F2}");
+            Console.WriteLine($"Truck: {truckFuel:F2}");
+        }
+    }
+}
