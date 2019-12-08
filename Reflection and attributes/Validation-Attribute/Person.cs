4bdf4e45a5cf5eec78abc126e6731875ee5ac5f0
P.M <vyfullspam50@gmail.com>
2019-12-08 23:23:38 +0200

    Add files via upload
    Exercise: Reflection and Attributes
    Problems for exercise and homework for the "C# OOP" course @ SoftUni".
    You can check your solutions here: Reflection and Attributes Exercise
    Reflection
    Command Pattern
    Skeleton provided
    Create a command pattern design using reflection. Use the provided skeleton, which contains a public StartUp class with a written logic inside the Main method. It is commented, so when you write the logic of your program, you can uncomment the code and test it. Input of commands will be received until "Exit" command. Each command line will look as it follows: "{CommandName} {CommandArgs}". CommandName will be as it follows: "Hello" -> executing HelloCommand and so on.
    There are a few steps you can follow in order to employ the command pattern design:
    Create a Command Interface
    Create a Command interface - ICommand, which contains a method - Execute(string[] args). 
    Create Command Objects
    For the purpose of this exercise, you need to create the following commands:
    HelloCommand - The result from its execution should be: $"Hello, {args[0]}".
    ExitCommand - It should exit the program and return null.
    Create a Command Interpreter Interface
    Create a command interpreter interface ICommandInterpreter, which contains a method Read(string args).
    Create a Command Interpreter Class
    Create a class CommandInterpreter. Its purpose is to read commands, execute them and return the result from their execution.  
    Create a Class Engine
    Create a class Engine, which contains a void Run() method. It should hold the following field:
    private readonly ICommandInterpreter commandInterpreter;
    It should have a constructor, which accepts an ICommandInterpreter: 
    public Engine(ICommandInterpreter commandInterpreter)
    The Run() method should accept input from the console and pass it to the proper class, as well as print the output from the commands. 
    Attributes
    Validation Attributes
    Skeleton provided
    Create your own custom validation attributes. There is a written logic inside the provided skeleton, which you can use to test the logic of your program. It is commented, so when you write the logic of your program, you can uncomment the code and test it.
    Create Attributes
    Create a validation attribute: MyValidationAttribute. Its purpose is to validate properties. 
    It should contain the following method: public abstract bool IsValid(object obj)
    Create a validation attribute: MyRangeAttribute.
    It's constructor should accept two parameters - int minValue, int maxValue, which represent a range of integer numbers
    It should contain two fields: int minValue and int maxValue
    It should implement the bool IsValid(object obj) method and its logic should validate whether the passed object obj parameter is within the set range
    Create a validation attribute: MyRequiredAttribute.
    It should implement the bool IsValid(object obj) method and its logic should validate wheter a property has the attribute or not
    Create an Entity
    Create a class Person. It should have a constructor, which accepts two parameters: string fullName, int age.
    It should have two properties:
    string FullName - the property is required. Apply the MyRequiredAttribute
    int Age - the age should be between 12 and 90. Apply the MyRangeAttribute and set the right values for minimum and maximum age
    Create a Validator Class
    Create a static class Validator. It should contain a method - public static bool IsValid(object obj), which must validate the properties of a given object.

---
+++ Reflection and attributes/Validation-Attribute/Person.cs
@@ -0,0 +1,32 @@
+﻿using static ValidationAttributes.MyRangeAttribute;
+
+namespace ValidationAttributes
+{
+    internal class Person
+    {
+		private string fullName;
+		private int age;
+
+		public Person( string name, int age)
+		{
+			FullName = name;
+			Age = age;
+		}
+
+		
+		[MyRange(12, 90)]
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+
+		[MyRequired]
+		public string FullName
+		{
+			get { return fullName; }
+			set { fullName = value; }
+		}
+
+	}
+}
---
+++ Reflection and attributes/Validation-Attribute/StartUp.cs
@@ -0,0 +1,95 @@
+﻿
+namespace ValidationAttributes
+{
+using System;
+using System.Linq;
+using System.Reflection;
+    using System.Collections.Generic;
+    using System.Collections;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person
+             (
+                 "Petar",
+                 11
+             );
+
+           bool isValidEntity = Validator.IsValid(person);
+
+            Console.WriteLine(isValidEntity); // True
+        }
+    }
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            bool result = false;
+            PropertyInfo[] propertyInfos = obj.GetType().GetProperties();
+
+            foreach (var propertyInfo in propertyInfos)
+            {
+                IEnumerable<MyValidationAttribute> attributes = propertyInfo.GetCustomAttributes()
+                    .Where(a => a is MyValidationAttribute)
+                    .Cast<MyValidationAttribute>();
+
+                foreach (var attribute in attributes)
+                {
+                    result = attribute.IsValid(propertyInfo.GetValue(obj));
+
+                    if (result == false) return result;
+                }
+            }
+
+            return result;
+        }
+    }
+    public abstract class MyValidationAttribute : Attribute
+    {
+        public abstract bool IsValid(object obj);
+
+    }
+
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private int minValue;
+
+        private int maxValue;
+
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+        public override bool IsValid(object obj)
+        {
+            if(obj is int valueAsInt)
+            {
+                if (valueAsInt >= minValue && valueAsInt <= maxValue)
+                {
+                    return true;
+                }
+                else return false;
+            }
+
+            throw new ArgumentException("Invalid type!") ;
+        }
+
+        public class MyRequiredAttribute : MyValidationAttribute
+        {
+            public override bool IsValid(object obj)
+            {
+                var result = false;
+                var entity = (string)obj;
+                if(obj as string != null)
+                {
+                    result = true;
+                }
+                return result;
+            }
+        }
+
+    }
+}
